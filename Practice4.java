385ddc23c21c34b1969e7c8e7618df657c799877
LRJ <1330234814@qq.com>
2021-03-09 08:57:23 +0800

    第二次提交

---
+++ Practice4.java
@@ -0,0 +1,10 @@
+public class Practice4 {
+    public static void main(String[] args) {
+        String A = "true";
+        String B = "this is my house";
+        boolean flagA = Boolean.parseBoolean(A);
+        boolean flagB = Boolean.parseBoolean(B);
+        System.out.println(flagA);
+        System.out.println(flagB);
+    }
+}
