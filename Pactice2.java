39cada9f1835808eff22297eaf25f41990780700
LRJ <1330234814@qq.com>
2021-03-09 09:30:43 +0800

    第三次提交

---
+++ Pactice2.java
@@ -0,0 +1,9 @@
+public class Pactice2 {
+    public static void main(String[] args) {
+        String str = "www";
+        String str1 = ".mdln";
+        String str2 = ".com";
+        System.out.println(str + str1 + str2);
+        System.out.println("魔力科技： \tMLDN\n在线学习网站: \"www.mldn.cn\"");
+    }
+}
