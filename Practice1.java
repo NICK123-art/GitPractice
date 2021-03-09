c5dcdb8169ca11cff01cc4b9663da13e6376a16d
LRJ <1330234814@qq.com>
2021-03-09 08:51:03 +0800

    第一次提交

---
+++ Practice1.java
@@ -0,0 +1,14 @@
+/**
+ * 该类的主要作用是在屏幕上输出信息
+ * @author LRJ123
+ */
+public class Practice1 {
+    public static void main(String[] args) {
+        /*
+         *【多行注释】下面的程序语句功能是在屏幕上打印一行输出信息
+         *利用多行注释可以针对代码的功能进行更加详细的说明
+         *在实际的项目开发中多行注释适合于编写大段的说明文字
+         */
+        System.out.println("课程资源请访问：www.mldn.cn");
+    }
+}
